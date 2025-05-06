8a1ee5a9fed7994de4efb88dc0c98441c0039d3d
Elizabeth Coats <escoats@unc.edu>
2025-05-06 13:34:28 -0400

    Scaffold home page

--- web/components/TaskCard.tsx
+++ web/components/TaskCard.tsx
@@ -1,34 +1,27 @@
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
-import { Checkbox } from "@/components/ui/checkbox"
-import { Label } from "@/components/ui/label"
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+import { Task } from "@/pages";
 
-export interface TaskProps{
-    title: string,
-
-}
-export default function TaskCard({title}: TaskProps) {
-    return (
-
-        <div className="border-blue-100 w-sm text-center">
-            <Card>
-                <CardHeader>
-                    <CardTitle>{title}</CardTitle>
-                </CardHeader>
-                <CardContent className="flex flex-row">
-                    <Checkbox className="m-4"></Checkbox>
-                    <Label>Done!</Label>
-
-                </CardContent>
-                
-            </Card>
-        </div>
-    )
-
+export default function TaskCard({ task }: Task) {
+  return (
+    <div className="border-blue-100 w-sm text-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>{task.title}</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-row">
+          <Checkbox className="m-4"></Checkbox>
+          <Label>Done!</Label>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,18 +1,92 @@
-import TaskCard from "@/components/TaskCard";
-import TaskProps from "@/components/TaskCard"
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useState } from "react";
 
+enum Priority {
+  HIGH,
+  MEDIUM,
+  LOW,
+}
+
+enum Status {
+  BACKLOG,
+  COMPLETE,
+}
+
+export interface Task {
+  id: number;
+  title: string;
+  description: string;
+  priority: Priority;
+  status: Status;
+}
 
 export default function Home() {
+  const task1: Task = {
+    id: 0,
+    title: "Organize Dresser",
+    description: "Clear out old clothes. Refold second and third drawers.",
+    priority: Priority.MEDIUM,
+    status: Status.BACKLOG,
+  };
+  const [activeTask, setActiveTask] = useState<Task | null>(task1);
 
-  
+  // TODO: implement
+  const handleClickSkip = () => {
+    // advance to next task
+  };
+
+  // TODO: implement
+  const handleClickDone = () => {
+    // edit corresponding task in dictionary (with matching id)
+    // to have status COMPLETE
+    // then advance to next task
+  };
   return (
     <>
-    <div className="text-3xl m-8">
-      <p>Hello!</p>
+      <p className="text-4xl text-center m-12">Task Tracker</p>
+      <div className="text-3xl m-8 flex flex-row content-center justify-center">
+        <Card className="w-md">
+          <CardHeader>
+            {activeTask ? (
+              <CardTitle>{activeTask.title}</CardTitle>
+            ) : (
+              <CardTitle>No more tasks!</CardTitle>
+            )}
+          </CardHeader>
+          <CardContent>
+            <CardDescription>
+              {activeTask ? <p>{activeTask.description}</p> : "Take a break!"}
+            </CardDescription>
+          </CardContent>
 
-      {/* Render cards in for loop*/}
-      <TaskCard title={"hello"} />
-    </div>
+          {activeTask && (
+            <CardFooter className="flex flex-row justify-between">
+              <Button
+                onClick={handleClickSkip}
+                variant="secondary"
+                className="bg-red-500"
+              >
+                Skip
+              </Button>
+              <Button
+                onClick={handleClickDone}
+                variant="default"
+                className="bg-emerald-500"
+              >
+                Done!
+              </Button>
+            </CardFooter>
+          )}
+        </Card>
+      </div>
     </>
   );
 }
